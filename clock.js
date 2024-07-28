41683f08304b1ada28ae3a2bcd64af33f54b1e72
Pankhuri Khurana <137212404+pankh23@users.noreply.github.com>
2024-07-28 12:13:50 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    // let a=new Date().toTimeString()
+    // timefs1text.innerHTML=a
+    let d=new Date().toDateString()
+    datefs1text.innerHTML=d
+},1000)
+
+setInterval(()=>{
+    let a=new Date().toTimeString()
+    timefs1text.innerHTML=a
+},1000)
+
+// document.getElementByClassName('clock')[0].innerText=time;
+
